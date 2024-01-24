10fd577454dbcf907bb8b7eca829a3ff87d9b31e
Moksh parashar <parasharm239@gmail.com>
2024-01-24 12:22:43 +0530

    .

--- app/components/Section.tsx
+++ app/components/Section.tsx
@@ -3,9 +3,9 @@ import React from "react";
 import img from "../../public/sectionImg.png";
 function Section() {
   return (
-    <section className="w-full mt-32 flex items-center justify-between px-72">
+    <section className="w-full mt-20 md:mt-0 flex items-center justify-between px-32 ">
       <div className="w-1/2">
-        <h1 className="text-blue-500 font-light text-4xl uppercase mb-4 tracking-widest">
+        <h1 className="text-white font-light text-4xl uppercase mb-4 tracking-widest">
           Manage your day with
         </h1>
         <span className="text-green-500 font-extrabold mt-4 text-6xl uppercase tracking-widest">
@@ -15,7 +15,7 @@ function Section() {
           your daily task manager | Let him assist you in your daily working.
         </p>
       </div>
-      <Image src={img} alt="" className="w-1/3 h-full" />
+      <Image src={img} alt="" className="w-1/2 h-1/2" />
     </section>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 
 import Section from "./components/Section";
+import Gradient from "./components/Gradient";
 
 export default function Home() {
   return (
     <main>
+      {/* <Gradient/> */}
       <Navbar />
       <Section />
     </main>
